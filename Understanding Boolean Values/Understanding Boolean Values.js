908bce778395be6c1f08155cd434706f45cef219
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-07-14 21:11:24 -0600

    a

---
+++ Understanding Boolean Values/Understanding Boolean Values.js
@@ -0,0 +1,11 @@
+function welcomeToBooleans(boolean) {
+    // Only change code below this line
+    if (boolean==1) {
+      return false;
+    }else{
+      return true;
+    }
+    // Change this line
+  
+    // Only change code above this line
+  }
