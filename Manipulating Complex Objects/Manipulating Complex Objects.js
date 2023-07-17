bedcc7abfce87f203baa6dd11166e5f86fbd4e38
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-07-17 16:09:20 -0600

    a

---
+++ Manipulating Complex Objects/Manipulating Complex Objects.js
@@ -0,0 +1,26 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ]
+    },
+    {
+      "artist": "Martin Garrix",
+      "title": "Animals",
+      "release_year": 2014,
+      "formats": ["CD", "Blue-Ray", "cassette"],
+      "gold": true
+    },
+    {
+      "artist": "Dua Lipa",
+      "title": "Nose",
+      "release_year": 2013,
+      "formats": ["CD", "Blue-Ray", "cassette"],
+      "gold": true
+    }
+  ];
